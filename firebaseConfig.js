467ace959e424105430f4be009b8f7e38beab2d0
Fatima0006 <fmendoza100642@ppsc.edu.mx>
2025-11-27 00:47:38 -0600

    Versión lista para Render

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAcz4kzlZtHr1BTbygWAx2vS0OHnwumG5I",
+  authDomain: "desmovil-70636.firebaseapp.com",
+  projectId: "desmovil-70636",
+  storageBucket: "desmovil-70636.firebasestorage.app",
+  messagingSenderId: "871424086259",
+  appId: "1:871424086259:web:a44577df397610e0326786"
+};
+
+module.exports = firebaseConfig;
